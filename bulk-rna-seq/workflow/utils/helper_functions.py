2642ccf0a62afe0e3e4485a33c8ad22432845c96
Julian Aileru <jaileru@uncc.edu>
2025-07-26 22:17:30 -0400

    Completed QC Module 07.26.25

--- bulk-rna-seq/workflow/utils/helper_functions.py
+++ bulk-rna-seq/workflow/utils/helper_functions.py
@@ -1,19 +1,7 @@
 import os 
+import re
 
-def get_paired_samples(folder='data/raw-reads'):
-    lst = os.listdir(folder)
-    global_dict = {}
-    for file in lst:
-        file_lst = file.split("_")
-        if file_lst[0] not in global_dict:
-            global_dict[file_lst[0]] = {'r1': None,
-                                    'r2': None}
-        if file_lst[1] == '1.fastq':
-            global_dict[file_lst[0]]['r1'] = os.path.join(folder,file)
-        if file_lst[1] == '2.fastq':
-            global_dict[file_lst[0]]['r2'] = os.path.join(folder,file)
-    return global_dict
-
-# paired_samples = get_paired_samples()
-# SAMPLES = list(paired_samples.keys())
-# print(paired_samples)
+def get_samples(folder):
+  lst = os.listdir(folder)
+  lst = [x.split("_")[0] for x in lst]
+  return list(set(lst))
